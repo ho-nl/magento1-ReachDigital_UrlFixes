e2654bf2bc0f3acf7b24d3230789f911cb7d4d9a
Hans Nieser <hans@reachdigital.nl>
2018-02-12 23:28:44 +0100

    [OPPO-2744] B: Don’t set our own duplication flag, one already exists

--- app/code/local/ReachDigital/UrlFixes/Model/Observer.php
+++ app/code/local/ReachDigital/UrlFixes/Model/Observer.php
@@ -3,19 +3,6 @@
 class ReachDigital_UrlFixes_Model_Observer extends Mage_Core_Model_Abstract
 {
     /**
-     * Set flag on duplicate product, so we can set a few things straight
-     *
-     * @event catalog_model_product_duplicate
-     * @param Varien_Event_Observer $observer
-     */
-    public function setDuplicationFlag(Varien_Event_Observer $observer)
-    {
-        /** @var Mage_Catalog_Model_Product $newProduct */
-        $newProduct = $observer->getNewProduct();
-        $newProduct->setData('reachdigital_urlfixer_is_duplicate', 1);
-    }
-
-    /**
      * Check if we're saving a duplicated product, and if so, clear url_key and product name.
      *
      * @event catalog_product_save_before
@@ -26,7 +13,7 @@ class ReachDigital_UrlFixes_Model_Observer extends Mage_Core_Model_Abstract
         /** @var Mage_Catalog_Model_Product $product */
         $product = $observer->getProduct();
 
-        if (!$product->getData('reachdigital_urlfixer_is_duplicate')) {
+        if (!$product->getIsDuplicate()) {
             return;
         }
 
