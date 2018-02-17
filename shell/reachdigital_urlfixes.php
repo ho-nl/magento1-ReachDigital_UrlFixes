eae4308b36cf21188f036c30280eb6d3a327ec9d
Hans Nieser <hans@reachdigital.nl>
2018-02-17 21:11:54 +0100

    [OPPO-2744] +: More shell actions

--- shell/reachdigital_urlfixes.php
+++ shell/reachdigital_urlfixes.php
@@ -4,14 +4,17 @@ require_once 'abstract.php';
 
 class Reachdigital_UrlFixes_Shell extends Mage_Shell_Abstract
 {
-    public function testAction()
+    public function dumpFallbackUrlMappingAction()
     {
-        $conn = Mage::getSingleton('core/resource')->getConnection('core_read');
-        $fallbackTable = $conn->getTableName('reachdigital_urlfixes_fallback');
-        $query = $conn->query(
+        $db = Mage::getSingleton('core/resource')->getConnection('core_read');
+        $fallbackTable = $db->getTableName('reachdigital_urlfixes_fallback');
+
+        // Get all rewrites in fallback table that are not also in the current rewrite table
+        $query = $db->query(
             "select fb.store_id, fb.request_path from `$fallbackTable` as fb
             left join core_url_rewrite as cur on cur.store_id = fb.store_id and cur.request_path = fb.request_path
-            where cur.url_rewrite_id is null");
+            where cur.url_rewrite_id is null
+            order by fb.store_id, fb.request_path");
 
         while ($url = $query->fetch()) {
             $oldUrl = $url['request_path'];
@@ -19,30 +22,68 @@ class Reachdigital_UrlFixes_Shell extends Mage_Shell_Abstract
             $newUrl = Mage::helper('reachdigital_urlfixes/url')->lookupFallbackRewrite($oldUrl, $storeId);
 
             if ($newUrl) {
-                echo "Mapping $oldUrl to $newUrl for store $storeId\n";
+                echo "store $storeId, fallback $oldUrl, target $newUrl\n";
             } else {
-                echo "Not found: $oldUrl for store $storeId\n";
+                echo "store $storeId, fallback $oldUrl, target not found\n";
             }
         }
     }
 
+    public function dumpFallbackUrlMappingActionHelp()
+    {
+        return ["Dump list of fallback URLs that no longer exist and the target URL (if any) they would be mapped to."];
+    }
+
     public function fillFallbackTableAction()
     {
-        $conn = Mage::getSingleton('core/resource')->getConnection('core_write');
-        $fallbackTable = $conn->getTableName('reachdigital_urlfixes_fallback');
+        $this->confirmOrExit(
+            "WARNING: This will truncate the fallback table and copy the current core_url_rewrite table into it.");
+
+        $db = Mage::getSingleton('core/resource')->getConnection('core_write');
+        $fallbackTable = $db->getTableName('reachdigital_urlfixes_fallback');
         $rewriteTable = Mage::getSingleton('core/resource')->getTableName('core/url_rewrite');
 
         $columns = "`url_rewrite_id`,`store_id`,`id_path`,`request_path`,`target_path`,`is_system`,`options`,`description`,`category_id`,`product_id`";
 
-        if ('y' !== readline("This will truncate the fallback table and fill it with the current rewrites, and can not be undone. Continue? ")) {
-            exit;
-        }
-
         echo "Truncating fallback table ($fallbackTable)\n";
-        $conn->query("TRUNCATE TABLE `$fallbackTable`");
+        $db->query("TRUNCATE TABLE `$fallbackTable`");
 
         echo "Copying values from $rewriteTable\n";
-        $conn->query("INSERT INTO `$fallbackTable` ($columns) SELECT $columns FROM `$rewriteTable`");
+        $db->query("INSERT INTO `$fallbackTable` ($columns) SELECT $columns FROM `$rewriteTable`");
+
+        echo "Done!\n";
+    }
+
+    public function fillFallbackTableActionHelp()
+    {
+        return ["Truncate current fallback table, and copy the current core_url_rewrite table into it."];
+    }
+
+    public function regenerateRewriteTableAction()
+    {
+        $this->confirmOrExit(
+            "WARNING: This will remove all product and category rewrites, and performs a full catalog URL reindex. ".
+            "Ensure that you have a full backup of your database, and have ran the 'fillFallbackTable' action first.");
+
+        // Delete all catalog rewrites
+        $db = Mage::getSingleton('core/resource')->getConnection('core_write');
+        $rewriteTable = $db->getTableName('core_url_rewrite');
+
+        $affected = $db->delete($rewriteTable, 'product_id IS NOT NULL OR category_id IS NOT NULL');
+        echo "Removed $affected existing rewrites.\n";
+
+        echo "Doing full reindex of catalog URL rewrites.\n";
+        Mage::getModel('catalog/indexer_url')->reindexAll();
+
+        echo "Done!\n";
+    }
+
+    public function regenerateRewriteTableActionHelp()
+    {
+        return [
+            "Remove all catalog URL rewrites (those which refer to a product and/or category) from the rewrite ",
+            "table and perform a full reindex."
+        ];
     }
 
     /*
@@ -52,6 +93,47 @@ class Reachdigital_UrlFixes_Shell extends Mage_Shell_Abstract
     public function restoreFallbackTableAction()
     {
         // TODO
+    }
+
+    public function restoreFallbackTableActionHelp()
+    {
+        return [
+            "Restore old URL rewrites by copying them from the fallback table to the core_url_rewrite table."
+        ];
+    }
+
+    /*
+     * Dump tab-separated overview of all conflicting URL keys.
+     */
+    public function dumpProductConflictsAction()
+    {
+        $conflicts = Mage::helper('reachdigital_urlfixes/url')->getAllProductUrlKeyConflicts();
+
+        foreach ($conflicts as $storeId => $keys) {
+            foreach ($keys as $key => $products) {
+                foreach ($products as $productId => $product) {
+                    $sku         = $product['sku'];
+                    $type        = $product['type'];
+                    $status      = $product['status'];
+                    $name        = $product['name'];
+                    $defaultName = $product['default_name'];
+                    echo "$storeId\t$key\t$productId\t$sku\t$type\t$status\t$name\t$defaultName\n";
+                }
+            }
+        }
+    }
+
+    public function dumpProductConflictsActionHelp()
+    {
+        return ["Dump all conflicting URL keys as tab-separated data (you might want to redirect this to a file)"];
+    }
+
+    protected function confirmOrExit($message)
+    {
+        echo "$message\n\n";
+        if ('y' !== readline("Continue? ")) {
+            exit;
+        }
     }
 
     /**
@@ -81,17 +163,26 @@ class Reachdigital_UrlFixes_Shell extends Mage_Shell_Abstract
      * @return string
      */
     public function usageHelp() {
-        $help = 'Available actions: ' . "\n";
+        $help = "Available actions:\n\n";
         $methods = get_class_methods($this);
         foreach ($methods as $method) {
-            if (substr($method, -6) == 'Action') {
-                $help .= '    -action ' . substr($method, 0, -6);
-                $helpMethod = $method.'Help';
-                if (method_exists($this, $helpMethod)) {
-                    $help .= '    ' . $this->$helpMethod();
-                }
-                $help .= "\n";
+            if (substr($method, -6) != 'Action') {
+                continue;
             }
+            $help .= '    -action ' . substr($method, 0, -6);
+            $helpMethod = $method.'Help';
+            if (method_exists($this, $helpMethod)) {
+                $extraHelp = $this->$helpMethod();
+                if (is_array($extraHelp)) {
+                    $help .= "\n";
+                    foreach ($extraHelp as $line) {
+                        $help .= "      $line\n";
+                    }
+                } else {
+                    $help .= "    $extraHelp";
+                }
+            }
+            $help .= "\n";
         }
         return $help;
     }
